c09a049314a8dab5c626dd39becad240ed2238cb
Floyd Jones <floyd-jones1@hotmail.com>
2021-08-04 00:04:55 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# `Create SWC App`
+
+A quick CLI tool to get you up and running with the blazing fast swc compiler.
+
+## Current supported Apps
+
+1. React (WIP)
+2. Vue (WIP)
+3. React Typescript
+4. Vue Typescript (WIP)
+
+## How to run?
+
+```
+npx create-swc-app --template=react-ts outDir
+```
+
+If you just run `npx create-swc-app` it will default to react typescript and create a folder called `my-app`
---
+++ src/index.js
@@ -0,0 +1,81 @@
+import fs from "fs";
+import path from "path";
+import chalk from "chalk";
+import ncp from "ncp";
+import { promisify } from "util";
+import { fileURLToPath } from "url";
+
+const access = promisify(fs.access);
+const copy = promisify(ncp);
+const log = console.log;
+
+const argList = [
+  { flag: "template", default: "react-ts", templateDir: "/template/react-ts" },
+];
+
+export const cli = (args) => {
+  const dest = args._[0] ? args._[0] : "my-app";
+
+  validateArgs(args);
+  const options = { targetDir: dest, ...getOptions(args) };
+
+  createProject(options).catch((err) => console.error(err));
+};
+
+const createProject = async (opts) => {
+  const templateDir = path.resolve(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "../templates",
+    opts.template.toLowerCase()
+  );
+  opts.templateDir = templateDir;
+
+  try {
+    await access(templateDir, fs.constants.R_OK);
+  } catch (err) {
+    log(chalk.bgRed(`\nInvalid tempalte directory`, err));
+    process.exit(1);
+  }
+
+  if (fs.existsSync(opts.targetDir)) {
+    log(
+      chalk.bgRed(
+        `\nFolder "${opts.targetDir}" already exist, please delete and run this command again\n`
+      )
+    );
+    process.exit(1);
+  }
+
+  await copyTemplateFiles(opts);
+};
+
+const getOptions = (args) => {
+  let options = {};
+  for (const arg of argList) {
+    if (!args[arg.flag]) {
+      options[arg.flag] = arg.default;
+      continue;
+    }
+    options[arg.flag] = args[arg.flag];
+  }
+  return options;
+};
+
+const validateArgs = (args) => {
+  const { _, ...rest } = args;
+  const userArgs = Object.keys(rest);
+  for (const userArg of userArgs) {
+    const found = argList.find((arg) => arg.flag === userArg);
+    if (!found) {
+      log(chalk.bgRed(`\nUnknown arg ${userArg}`));
+      process.exit();
+    }
+  }
+};
+
+const copyTemplateFiles = (opts) => {
+  return copy(opts.templateDir, opts.targetDir, {
+    clobber: false,
+    stopOnErr: true,
+  });
+};
